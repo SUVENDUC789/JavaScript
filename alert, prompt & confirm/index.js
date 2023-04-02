2b19f85a976299f6922ec0adc924fd3e893367df
Suvendu <suvendu.chok@gmail.com>
2023-04-02 19:15:51 +0530

    all about alert, prompt & confirm

---
+++ alert, prompt & confirm/index.js
@@ -0,0 +1,6 @@
+console.info("alert, prompt & confirm");
+alert("Enter the value of a : ");
+let a = prompt("Enter the value of a : ")
+a = confirm("Chose one .");
+document.write(a);
+console.log(document);
