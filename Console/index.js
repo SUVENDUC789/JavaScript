cd4cca55685b17c77a9ecf762f52dd515611d85e
Suvendu <suvendu.chok@gmail.com>
2023-04-02 18:59:19 +0530

    all about console object

---
+++ Console/index.js
@@ -0,0 +1,15 @@
+console.time("suvu");
+console.log("All about of console.");
+console.error("This is error message");
+console.log(console);
+const a = {
+    suvendu: 412,
+    hritik: 420,
+    supratim: 411,
+    debarpan: 440,
+};
+
+console.table(a);
+console.warn("I am a warning.");
+console.info("For your kind information.");
+console.timeEnd("suvu");
