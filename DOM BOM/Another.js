adc90f6e87b13d5410ebe95d15abfaa4226862bb
Suvendu <suvendu.chok@gmail.com>
2023-04-26 10:15:57 +0530

    All about besic dom

---
+++ DOM BOM/Another.js
@@ -0,0 +1,14 @@
+let vol = 0;
+let increment = document.getElementById("incre-btn");
+let decrement = document.getElementById("decre-btn");
+let heading = document.getElementById("heading");
+
+increment.addEventListener('click', function () {
+    vol++;
+    heading.innerHTML = 'Volume up ' + vol + ' '
+});
+
+decrement.addEventListener('click', () => {
+    vol--;
+    heading.innerHTML = 'Volume down ' + vol + ' '
+});
---
+++ DOM BOM/index.js
@@ -0,0 +1,13 @@
+console.log("DOM, BOM & Window Object ");
+
+const dbom = {
+
+    BOM: "Browser ObJect Model",
+    DOM: "Document Object Model",
+};
+
+console.log(dbom);
+window.console.log(window);
+console.log(document.body);
+console.log(document.body.style.background="green");
+console.log(document.body.style.color="white");
